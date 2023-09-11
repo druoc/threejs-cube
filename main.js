7b49f2346e8ade58cf3e12cad31288b29819d6b0
Dave <d.ruocco1987@googlemail.com>
2023-09-11 13:40:15 +0100

    Adds rotation

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ document.body.appendChild(renderer.domElement);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
+  wireframe: true,
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
@@ -23,6 +24,8 @@ camera.position.z = 5;
 
 const animate = () => {
   requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 };
 animate();
